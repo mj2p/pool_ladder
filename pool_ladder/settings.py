eda38a4eec379ecc8e3bd777abcc5a68a3cd06d1
sammoth-ripjar <sam.griffiths@ripjar.com>
2019-01-18 10:25:35 +0000

    configure ALLOWED_HOSTS

--- pool_ladder/settings.py
+++ pool_ladder/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = env['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'pool-ladder.herokuapp.com'
+]
 
 
 # Application definition
